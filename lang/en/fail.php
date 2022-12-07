73be98030e1fc1f443d33347de3577ba39e3ac67
leenaa46 <hsherlock22@gmail.com>
2022-12-07 16:23:22 +0700

    Test

--- lang/en/fail.php
+++ lang/en/fail.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
-    'data_in_used' => 'Error, this ":Data" is in used by another ":Other".'
+    'data_in_used' => 'Error, this ":Data" is in used by another ":Other".',
+    'route_not_found' => "Error, there is no route with the given path."
 ];
